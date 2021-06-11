99fb88c76b3d5dc6172a308058089d744575a33c
prono69 <faltu9851@gmail.com>
2021-06-11 18:26:00 +0530

    fixed butts json error. added improved doge command :)

--- userbot/plugins/nsfw.py
+++ userbot/plugins/nsfw.py
@@ -112,7 +112,7 @@ async def butts(e):
     a = await edit_or_reply(e, "`Finding some beautiful butts...`")
     await sleep(1)
     await a.edit("`Sending some beautiful butts...`")
-    nsfw = requests.get("http://api.obutts.ru/noise/1").json()[0]["preview"]
+    nsfw = requests.get("http://api.obutts.ru/butts/noise/1").json()[0]["preview"]
     urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), "*.jpg")
     os.rename("*.jpg", "butts.jpg")
     await e.client.send_file(e.chat_id, "butts.jpg")
