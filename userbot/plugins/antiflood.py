f3c76e07d7eb2ad96437f3755bd0cacced203cfc
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-09 19:35:29 +0530

    Update antiflood.py

--- userbot/plugins/antiflood.py
+++ userbot/plugins/antiflood.py
@@ -36,20 +36,20 @@ async def _(event):
     except Exception as e:
         no_admin_privilege_message = await event.client.send_message(
             entity=event.chat_id,
-            message=f"**Automatic AntiFlooder**\x1f@admin [User](tg://user?id={event.message.sender_id}) is flooding this chat.\x1f`{e}`",
+            message=f"**Err automatic anti flooder**\x1f@admin [User](tg://user?id={event.message.sender_id}) is flooding this chat\x1f`{e}`",
             reply_to=event.message.id,
         )
 
         await asyncio.sleep(4)
         await no_admin_privilege_message.edit(
-            "This is useless SPAM dude. Stop this, enjoy the chat buddy "
+            "This is useless spam dude ! Stop this , enjoy the chat buddy"
         )
     else:
         await event.client.send_message(
             entity=event.chat_id,
-            message=f"""**Automatic AntiFlooder**
+            message=f"""**Err automatic anti flooder**
 [User](tg://user?id={event.message.sender_id}) has been automatically restricted
-because he reached the defined flood limit.""",
+because he reached the defined flood limit""",
             reply_to=event.message.id,
         )
 
@@ -59,7 +59,7 @@ because he reached the defined flood limit.""",
     command=("setflood", plugin_category),
     info={
         "header": "To setup antiflood in a group",
-        "description": "It warns the user if he spams the chat and if you are an admin with proper rights then it mutes him in that group.",
+        "description": "It warns the user if he spams the chat and if you are an admin with proper rights then it mutes him in that group",
         "note": "To stop antiflood setflood with high value like 999999",
         "usage": "{tr}setflood <count>",
         "examples": [
@@ -72,11 +72,11 @@ because he reached the defined flood limit.""",
 async def _(event):
     "To setup antiflood in a group to prevent spam"
     input_str = event.pattern_match.group(1)
-    event = await edit_or_reply(event, "`updating flood settings!`")
+    event = await edit_or_reply(event, "`Updating flood settings`")
     await asyncio.sleep(2)
     try:
         sql.set_flood(event.chat_id, input_str)
         sql.__load_flood_settings()
-        await event.edit(f"Antiflood updated to {input_str} in the current chat")
+        await event.edit(f"Anti flood updated to {input_str} in the current chat")
     except Exception as e:
         await event.edit(str(e))
