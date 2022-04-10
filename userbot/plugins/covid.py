d3494298835cf31f23d550ad1a8268a70e513be5
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-10 14:24:44 +0530

    Update covid.py

--- userbot/plugins/covid.py
+++ userbot/plugins/covid.py
@@ -10,17 +10,17 @@ plugin_category = "extra"
     pattern="covid(?:\s|$)([\s\S]*)",
     command=("covid", plugin_category),
     info={
-        "header": "To get latest information about covid-19.",
-        "description": "Get information about covid-19 data in the given country/state(only Indian States).",
+        "header": "To get latest information about covid-19",
+        "description": "Get information about covid-19 data in the given country or state ( only indian states )",
         "usage": "{tr}covid <state_name/country_name>",
         "examples": ["{tr}covid andhra pradesh", "{tr}covid india", "{tr}covid world"],
     },
 )
 async def corona(event):
-    "To get latest information about covid-19."
+    "To get latest information about covid-19"
     input_str = event.pattern_match.group(1)
     country = (input_str).title() if input_str else "World"
-    catevent = await edit_or_reply(event, "`Collecting data...`")
+    catevent = await edit_or_reply(event, "`Collecting data`")
     covid = Covid(source="worldometers")
     try:
         country_data = covid.get_status_by_country_name(country)
@@ -30,16 +30,16 @@ async def corona(event):
         hmm1 = country_data["confirmed"] + country_data["new_cases"]
         hmm2 = country_data["deaths"] + country_data["new_deaths"]
         data = ""
-        data += f"\n⚠️ Confirmed   : <code>{hmm1}</code>"
-        data += f"\n😔 Active           : <code>{country_data['active']}</code>"
-        data += f"\n⚰️ Deaths         : <code>{hmm2}</code>"
-        data += f"\n🤕 Critical          : <code>{country_data['critical']}</code>"
-        data += f"\n😊 Recovered   : <code>{country_data['recovered']}</code>"
-        data += f"\n💉 Total tests    : <code>{country_data['total_tests']}</code>"
-        data += f"\n🥺 New Cases   : <code>{country_data['new_cases']}</code>"
-        data += f"\n😟 New Deaths : <code>{country_data['new_deaths']}</code>"
+        data += f"\n🤒 Confirmed : <code>{hmm1}</code>"
+        data += f"\n😓 Active : <code>{country_data['active']}</code>"
+        data += f"\n😵 Deaths : <code>{hmm2}</code>"
+        data += f"\n🤕 Critical : <code>{country_data['critical']}</code>"
+        data += f"\n☺️ Recovered : <code>{country_data['recovered']}</code>"
+        data += f"\n💉 Total tests : <code>{country_data['total_tests']}</code>"
+        data += f"\n🥺 New cases : <code>{country_data['new_cases']}</code>"
+        data += f"\n😖 New deaths : <code>{country_data['new_deaths']}</code>"
         await catevent.edit(
-            "<b>Corona Virus Info of {}:\n{}</b>".format(country, data),
+            "<b>Corona virus info of {}:\n{}</b>".format(country, data),
             parse_mode="html",
         )
     else:
@@ -49,18 +49,18 @@ async def corona(event):
             cat2 = int(data["new_death"]) - int(data["death"])
             cat3 = int(data["new_cured"]) - int(data["cured"])
             result = f"<b>Corona virus info of {data['state_name']}\
-                \n\n⚠️ Confirmed   : <code>{data['new_positive']}</code>\
-                \n😔 Active           : <code>{data['new_active']}</code>\
-                \n⚰️ Deaths         : <code>{data['new_death']}</code>\
-                \n😊 Recovered   : <code>{data['new_cured']}</code>\
-                \n🥺 New Cases   : <code>{cat1}</code>\
-                \n😟 New Deaths : <code>{cat2}</code>\
-                \n😃 New cured  : <code>{cat3}</code> </b>"
+                \n\n🤒 Confirmed : <code>{data['new_positive']}</code>\
+                \n😓 Active : <code>{data['new_active']}</code>\
+                \n😵 Deaths : <code>{data['new_death']}</code>\
+                \n☺️ Recovered : <code>{data['new_cured']}</code>\
+                \n🥺 New cases : <code>{cat1}</code>\
+                \n😖 New deaths : <code>{cat2}</code>\
+                \n😍 New cured : <code>{cat3}</code> </b>"
             await catevent.edit(result, parse_mode="html")
         else:
             await edit_delete(
                 catevent,
-                "`Corona Virus Info of {} is not avaiable or unable to fetch`".format(
+                "`Corona virus info of {} is not available or unable to fetch`".format(
                     country
                 ),
                 5,
