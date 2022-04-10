bfc8e71f6df09c908c20f10e0d7756c28c2acf1d
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-10 14:07:26 +0530

    Update corecmds.py

--- userbot/plugins/corecmds.py
+++ userbot/plugins/corecmds.py
@@ -15,13 +15,13 @@ thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg"
     pattern="install$",
     command=("install", plugin_category),
     info={
-        "header": "To install an external plugin.",
-        "description": "Reply to any external plugin(supported by cat) to install it in your bot.",
+        "header": "To install an external plugin",
+        "description": "Reply to any external plugin ( supported by cat ) to install it in your bot",
         "usage": "{tr}install",
     },
 )
 async def install(event):
-    "To install an external plugin."
+    "To install an external plugin"
     if event.reply_to_msg_id:
         try:
             downloaded_file_name = await event.client.download_media(
@@ -34,31 +34,31 @@ async def install(event):
                 load_module(shortname.replace(".py", ""))
                 await edit_delete(
                     event,
-                    f"Installed Plugin `{os.path.basename(downloaded_file_name)}`",
+                    f"Installed plugin `{os.path.basename(downloaded_file_name)}`",
                     10,
                 )
             else:
                 os.remove(downloaded_file_name)
                 await edit_delete(
-                    event, "Errors! This plugin is already installed/pre-installed.", 10
+                    event, "Error this plugin is already pre-installed", 10
                 )
         except Exception as e:
-            await edit_delete(event, f"**Error:**\n`{e}`", 10)
+            await edit_delete(event, f"**Error :**\n`{e}`", 10)
             os.remove(downloaded_file_name)
 
 
 @catub.cat_cmd(
     pattern="load ([\s\S]*)",
-    command=("load", plugin_category),
+    command=("load", plugin_category), 
     info={
-        "header": "To load a plugin again. if you have unloaded it",
+        "header": "To load a plugin again if you have unloaded it",
         "description": "To load a plugin again which you unloaded by {tr}unload",
         "usage": "{tr}load <plugin name>",
         "examples": "{tr}load markdown",
     },
 )
 async def load(event):
-    "To load a plugin again. if you have unloaded it"
+    "To load a plugin again if you have unloaded it"
     shortname = event.pattern_match.group(1)
     try:
         try:
@@ -70,7 +70,7 @@ async def load(event):
     except Exception as e:
         await edit_or_reply(
             event,
-            f"Could not load {shortname} because of the following error.\n{e}",
+            f"Could not load {shortname} because of the following error\n{e}",
         )
 
 
@@ -97,25 +97,25 @@ async def send(event):
             allow_cache=False,
             reply_to=reply_to_id,
             thumb=thumb,
-            caption=f"**➥ Plugin Name:-** `{input_str}`",
+            caption=f"**Plugin name :-** `{input_str}`",
         )
         await event.delete()
     else:
-        await edit_or_reply(event, "404: File Not Found")
+        await edit_or_reply(event, "404 : File not found")
 
 
 @catub.cat_cmd(
     pattern="unload ([\s\S]*)",
     command=("unload", plugin_category),
     info={
-        "header": "To unload a plugin temporarily.",
-        "description": "You can load this unloaded plugin by restarting or using {tr}load cmd. Useful for cases like seting notes in rose bot({tr}unload markdown).",
+        "header": "To unload a plugin temporarily",
+        "description": "You can load this unloaded plugin by restarting or using {tr} load command , useful for cases like seting notes in rose bot ({tr} unload markdown )",
         "usage": "{tr}unload <plugin name>",
         "examples": "{tr}unload markdown",
     },
 )
 async def unload(event):
-    "To unload a plugin temporarily."
+    "To unload a plugin temporarily"
     shortname = event.pattern_match.group(1)
     try:
         remove_plugin(shortname)
@@ -128,15 +128,15 @@ async def unload(event):
     pattern="uninstall ([\s\S]*)",
     command=("uninstall", plugin_category),
     info={
-        "header": "To uninstall a plugin temporarily.",
-        "description": "To stop functioning of that plugin and remove that plugin from bot.",
-        "note": "To unload a plugin permanently from bot set NO_LOAD var in heroku with that plugin name, give space between plugin names if more than 1.",
+        "header": "To uninstall a plugin temporarily",
+        "description": "To stop functioning of that plugin and remove that plugin from bot",
+        "note": "To unload a plugin permanently from bot set NO_LOAD var in heroku with that plugin name , give space between plugin names if more than 1",
         "usage": "{tr}uninstall <plugin name>",
         "examples": "{tr}uninstall markdown",
     },
 )
 async def unload(event):
-    "To uninstall a plugin."
+    "To uninstall a plugin"
     shortname = event.pattern_match.group(1)
     path = Path(f"userbot/plugins/{shortname}.py")
     if not os.path.exists(path):
