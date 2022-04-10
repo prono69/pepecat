d3e05840dbde35b6cd41231cf3547ce631149203
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-10 10:46:35 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -18,7 +18,7 @@ CARBONLANG = "auto"
     pattern="carbon(?:\s|$)([\s\S]*)",
     command=("carbon", plugin_category),
     info={
-        "header": "Carbon generators for given text (Fixed style)",
+        "header": "Carbon generators for given text ( fixed style )",
         "usage": [
             "{tr}carbon <text>",
             "{tr}carbon <reply to text>",
@@ -26,8 +26,8 @@ CARBONLANG = "auto"
     },
 )
 async def carbon_api(event):
-    """A Wrapper for carbon.now.sh"""
-    await event.edit("`Processing..`")
+    """A wrapper for carbon.now.sh"""
+    await event.edit("`Processing.. .. .. ..`")
     CARBON = "https://carbon.now.sh/?l={lang}&code={code}"
     textx = await event.get_reply_message()
     pcode = event.text
@@ -37,22 +37,22 @@ async def carbon_api(event):
         pcode = str(textx.message)
     pcode = deEmojify(pcode)
     code = quote_plus(pcode)
-    cat = await edit_or_reply(event, "`Carbonizing...\n25%`")
+    cat = await edit_or_reply(event, "`Carbonizing.. .. .. ..\n25%`")
     url = CARBON.format(code=code, lang=CARBONLANG)
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--Headless")
     chrome_options.binary_location = Config.CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--Window-size=1920x1080")
+    chrome_options.add_argument("--Disable-dev-shm-usage")
+    chrome_options.add_argument("--No-sandbox")
+    chrome_options.add_argument("--Disable-gpu")
     prefs = {"download.default_directory": "./"}
     chrome_options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(
         executable_path=Config.CHROME_DRIVER, options=chrome_options
     )
     driver.get(url)
-    await cat.edit("`Be Patient...\n50%`")
+    await cat.edit("`Be patient...\n50%`")
     download_path = "./"
     driver.command_executor._commands["send_command"] = (
         "POST",
@@ -68,7 +68,7 @@ async def carbon_api(event):
     await cat.edit("`Processing..\n75%`")
 
     await asyncio.sleep(2)
-    await cat.edit("`Done Dana Done...\n100%`")
+    await cat.edit("`Done danaa done\n100%`")
     file = "./carbon.png"
     await cat.edit("`Uploading..`")
     await event.client.send_file(
@@ -88,7 +88,7 @@ async def carbon_api(event):
     pattern="krb(?:\s|$)([\s\S]*)",
     command=("krb", plugin_category),
     info={
-        "header": "Carbon generators for given text. each time gives  random style. You can also use patcicular style by using semicolon after text and name",
+        "header": "Carbon generators for given text , each time gives  random style ! You can also use patcicular style by using semicolon after text and name",
         "usage": [
             "{tr}krb <text>",
             "{tr}krb <reply to text>",
@@ -97,8 +97,8 @@ async def carbon_api(event):
     },
 )
 async def carbon_api(event):
-    """A Wrapper for carbon.now.sh"""
-    cat = await edit_or_reply(event, "`Processing....`")
+    """A wrapper for carbon.now.sh"""
+    cat = await edit_or_reply(event, "`Processing.. .. .. ..`")
     CARBON = "https://carbon.now.sh/?l={lang}&code={code}"
     textx = await event.get_reply_message()
     pcode = event.text
@@ -116,22 +116,22 @@ async def carbon_api(event):
     skeme = skeme.strip()
     pcode = deEmojify(pcode)
     code = quote_plus(pcode)
-    await cat.edit("`Meking Carbon...`\n`25%`")
+    await cat.edit("`Making carbon...`\n`25%`")
     url = CARBON.format(code=code, lang=CARBONLANG)
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--Headless")
     chrome_options.binary_location = Config.CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--Window-size=1920x1080")
+    chrome_options.add_argument("--Disable-dev-shm-usage")
+    chrome_options.add_argument("--No-sandbox")
+    chrome_options.add_argument("--Disable-gpu")
     prefs = {"download.default_directory": "./"}
     chrome_options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(
         executable_path=Config.CHROME_DRIVER, options=chrome_options
     )
     driver.get(url)
-    await cat.edit("`Be Patient...\n50%`")
+    await cat.edit("`Be patient...\n50%`")
     download_path = "./"
     driver.command_executor._commands["send_command"] = (
         "POST",
@@ -164,13 +164,13 @@ async def carbon_api(event):
     color_name = driver.find_element_by_xpath(
         "/html/body/div[1]/main/div[3]/div[2]/div[1]/div[1]/div/span[2]/input"
     ).get_attribute("value")
-    await cat.edit("`Done Dana Done...\n100%`")
+    await cat.edit("`Done danaa done\n100%`")
     file = "./carbon.png"
     await cat.edit("`Uploading..`")
     await event.client.send_file(
         event.chat_id,
         file,
-        caption=f"`Here's your carbon!` \n**Colour Scheme: **`{color_name}`",
+        caption=f"`Here's your carbon !` \n**Colour scheme : **`{color_name}`",
         force_document=True,
         reply_to=event.message.reply_to_msg_id,
     )
@@ -183,7 +183,7 @@ async def carbon_api(event):
     pattern="kar1(?:\s|$)([\s\S]*)",
     command=("kar1", plugin_category),
     info={
-        "header": "Carbon generators for given text (Fixed style)",
+        "header": "Carbon generators for given text ( fixed style )",
         "usage": [
             "{tr}kar1 <text>",
             "{tr}kar1 <reply to text>",
@@ -191,7 +191,7 @@ async def carbon_api(event):
     },
 )
 async def carbon_api(event):
-    """A Wrapper for carbon.now.sh"""
+    """A wrapper for carbon.now.sh"""
     cat = await edit_or_reply(event, "🔲🔲🔲🔲🔲")
     CARBON = "https://carbon.now.sh/?bg=rgba(249%2C237%2C212%2C0)&t=synthwave-84&wt=none&l=application%2Fjson&ds=true&dsyoff=20px&dsblur=0px&wc=true&wa=true&pv=56px&ph=0px&ln=false&fl=1&fm=IBM%20Plex%20Mono&fs=14.5px&lh=153%25&si=false&es=4x&wm=false&code={code}"
     textx = await event.get_reply_message()
@@ -205,10 +205,10 @@ async def carbon_api(event):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.binary_location = Config.CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--Window-size=1920x1080")
+    chrome_options.add_argument("--Disable-dev-shm-usage")
+    chrome_options.add_argument("--No-sandbox")
+    chrome_options.add_argument("--Disable-gpu")
     prefs = {"download.default_directory": "./"}
     chrome_options.add_experimental_option("prefs", prefs)
     await cat.edit("🔳🔳🔲🔲🔲")
@@ -234,7 +234,7 @@ async def carbon_api(event):
     await asyncio.sleep(1)
     await cat.edit("🔳🔳🔳🔳🔳")
     file = "./carbon.png"
-    await cat.edit("☣️Karbon1 Completed, Uploading Karbon☣️")
+    await cat.edit("Carbon1 completed , uploading carbon")
     await event.client.send_file(
         event.chat_id,
         file,
@@ -250,7 +250,7 @@ async def carbon_api(event):
     pattern="kar2(?:\s|$)([\s\S]*)",
     command=("kar2", plugin_category),
     info={
-        "header": "Carbon generators for given text (Fixed style)",
+        "header": "Carbon generators for given text ( fixed style )",
         "usage": [
             "{tr}kar2 <text>",
             "{tr}kar2 <reply to text>",
@@ -258,7 +258,7 @@ async def carbon_api(event):
     },
 )
 async def carbon_api(event):
-    """A Wrapper for carbon.now.sh"""
+    """A wrapper for carbon.now.sh"""
     cat = await edit_or_reply(event, "📛📛📛📛📛")
     CARBON = "https://carbon.now.sh/?bg=rgba(239%2C40%2C44%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}"
     textx = await event.get_reply_message()
@@ -270,12 +270,12 @@ async def carbon_api(event):
     code = quote_plus(pcode)
     url = CARBON.format(code=code, lang=CARBONLANG)
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--Headless")
     chrome_options.binary_location = Config.CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--Window-size=1920x1080")
+    chrome_options.add_argument("--Disable-dev-shm-usage")
+    chrome_options.add_argument("--No-sandbox")
+    chrome_options.add_argument("--Disable-gpu")
     prefs = {"download.default_directory": "./"}
     chrome_options.add_experimental_option("prefs", prefs)
     await cat.edit("🔘🔘📛📛📛")
@@ -299,11 +299,11 @@ async def carbon_api(event):
     await asyncio.sleep(1)
     await cat.edit("🔘🔘🔘🔘🔘")
     file = "./carbon.png"
-    await cat.edit("☣️Karbon2 Completed, Uploading Karbon☣️")
+    await cat.edit("Carbon2 completed , uploading carbon")
     await event.client.send_file(
         event.chat_id,
         file,
-        caption="Here's your Karbon2",
+        caption="Here's your carbon2",
         force_document=True,
         reply_to=event.message.reply_to_msg_id,
     )
@@ -317,7 +317,7 @@ async def carbon_api(event):
     pattern="kar3(?:\s|$)([\s\S]*)",
     command=("kar3", plugin_category),
     info={
-        "header": "Carbon generators for given text (Fixed style)",
+        "header": "Carbon generators for given text ( Fixed style )",
         "usage": [
             "{tr}kar3 <text>",
             "{tr}kar3 <reply to text>",
@@ -325,7 +325,7 @@ async def carbon_api(event):
     },
 )
 async def carbon_api(event):
-    """A Wrapper for carbon.now.sh"""
+    """A wrapper for carbon.now.sh"""
     cat = await edit_or_reply(event, "🎛🎛🎛🎛🎛")
     CARBON = "https://carbon.now.sh/?bg=rgba(74%2C144%2C226%2C1)&t=material&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Fira%20Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code={code}"
     textx = await event.get_reply_message()
@@ -337,12 +337,12 @@ async def carbon_api(event):
     code = quote_plus(pcode)
     url = CARBON.format(code=code, lang=CARBONLANG)
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--Headless")
     chrome_options.binary_location = Config.CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--Window-size=1920x1080")
+    chrome_options.add_argument("--Disable-dev-shm-usage")
+    chrome_options.add_argument("--No-sandbox")
+    chrome_options.add_argument("--Disable-gpu")
     prefs = {"download.default_directory": "./"}
     chrome_options.add_experimental_option("prefs", prefs)
     await cat.edit("🔵🔵🎛🎛🎛")
@@ -368,11 +368,11 @@ async def carbon_api(event):
     await asyncio.sleep(1)
     await cat.edit("🔵🔵🔵🔵🔵")
     file = "./carbon.png"
-    await cat.edit("☣️Karbon3 Completed, Uploading Karbon⬆️")
+    await cat.edit("Carbon3 completed , uploading carbon")
     await event.client.send_file(
         event.chat_id,
         file,
-        caption="Here's your Karbon3",
+        caption="Here's your carbon3",
         force_document=True,
         reply_to=event.message.reply_to_msg_id,
     )
@@ -385,7 +385,7 @@ async def carbon_api(event):
     pattern="kar4(?:\s|$)([\s\S]*)",
     command=("kar4", plugin_category),
     info={
-        "header": "Carbon generators for given text (Fixed style)",
+        "header": "Carbon generators for given text ( fixed style )",
         "usage": [
             "{tr}kar4 <text>",
             "{tr}kar4 <reply to text>",
@@ -393,7 +393,7 @@ async def carbon_api(event):
     },
 )
 async def carbon_api(event):
-    """A Wrapper for carbon.now.sh"""
+    """A wrapper for carbon.now.sh"""
     cat = await edit_or_reply(event, "🌚🌚🌚🌚🌚")
     CARBON = "https://carbon.now.sh/?bg=rgba(29%2C40%2C104%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}"
     textx = await event.get_reply_message()
@@ -405,12 +405,12 @@ async def carbon_api(event):
     code = quote_plus(pcode)
     url = CARBON.format(code=code, lang=CARBONLANG)
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--Headless")
     chrome_options.binary_location = Config.CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--Window-size=1920x1080")
+    chrome_options.add_argument("--Disable-dev-shm-usage")
+    chrome_options.add_argument("--No-sandbox")
+    chrome_options.add_argument("--Disable-gpu")
     prefs = {"download.default_directory": "./"}
     chrome_options.add_experimental_option("prefs", prefs)
     await cat.edit("🌝🌝🌚🌚🌚")
@@ -434,11 +434,11 @@ async def carbon_api(event):
     await asyncio.sleep(1)
     await cat.edit("🌝🌝🌝🌝🌝")
     file = "./carbon.png"
-    await cat.edit("✅Karbon4 Completed, Uploading Karbon✅")
+    await cat.edit("Carbon4 completed , uploading carbon")
     await event.client.send_file(
         event.chat_id,
         file,
-        caption="Here's your Karbon4 ",
+        caption="Here's your carbon4 ",
         force_document=True,
         reply_to=event.message.reply_to_msg_id,
     )
@@ -451,7 +451,7 @@ async def carbon_api(event):
     pattern="kargb(?:\s|$)([\s\S]*)",
     command=("kargb", plugin_category),
     info={
-        "header": "Carbon generators for given text (random from some selected themes)",
+        "header": "Carbon generators for given text ( random from some selected themes )",
         "usage": [
             "{tr}kargb <text>",
             "{tr}kargb <reply to text>",
@@ -459,7 +459,7 @@ async def carbon_api(event):
     },
 )
 async def carbon_api(event):
-    """A Wrapper for carbon.now.sh"""
+    """A wrapper for carbon.now.sh"""
     RED = random.randint(0, 256)
     GREEN = random.randint(0, 256)
     BLUE = random.randint(0, 256)
@@ -507,12 +507,12 @@ async def carbon_api(event):
     code = quote_plus(pcode)
     url = CARBON.format(code=code, R=RED, G=GREEN, B=BLUE, T=The, lang=CARBONLANG)
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--Headless")
     chrome_options.binary_location = Config.CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--Window-size=1920x1080")
+    chrome_options.add_argument("--Disable-dev-shm-usage")
+    chrome_options.add_argument("--No-sandbox")
+    chrome_options.add_argument("--Disable-gpu")
     prefs = {"download.default_directory": "./"}
     chrome_options.add_experimental_option("prefs", prefs)
     await cat.edit("⬛⬛⬜⬜⬜")
@@ -536,11 +536,11 @@ async def carbon_api(event):
     await asyncio.sleep(1)
     await cat.edit("⬛⬛⬛⬛⬛")
     file = "./carbon.png"
-    await cat.edit("✅RGB Karbon Completed, Uploading Karbon✅")
+    await cat.edit("Rgb carbon completed , uploading carbon")
     await event.client.send_file(
         event.chat_id,
         file,
-        caption="Here's your karbonrgb",
+        caption="Here's your carbonrgb",
         force_document=True,
         reply_to=event.message.reply_to_msg_id,
     )
