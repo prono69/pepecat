acce4b01bb3e25352f6e88519cc3bce4d3aef0c1
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-11 13:51:25 +0530

    Update filext.py

--- userbot/plugins/filext.py
+++ userbot/plugins/filext.py
@@ -12,13 +12,13 @@ plugin_category = "utils"
     pattern="filext(?:\s|$)([\s\S]*)",
     command=("filext", plugin_category),
     info={
-        "header": "Shows you the detailed information of given extension type.",
+        "header": "Shows you the detailed information of given extension type",
         "usage": "{tr}filext <extension>",
         "examples": "{tr}filext py",
     },
 )
 async def _(event):
-    "Shows you the detailed information of given extension type."
+    "Shows you the detailed information of given extension type"
     sample_url = "https://www.fileext.com/file-extension/{}.html"
     input_str = event.pattern_match.group(1).lower()
     response_api = requests.get(sample_url.format(input_str))
@@ -29,10 +29,10 @@ async def _(event):
         ext_details = soup.find_all("td", {"colspan": "3"})[-1].text
         await edit_or_reply(
             event,
-            f"**File Extension**: `{input_str}`\n**Description**: `{ext_details}`",
+            f"**File extension** : `{input_str}`\n**Description** : `{ext_details}`",
         )
     else:
         await edit_or_reply(
             event,
-            f"https://www.fileext.com/ responded with {status_code} for query: {input_str}",
+            f"https://www.fileext.com responded with {status_code} for query: {input_str}",
         )
