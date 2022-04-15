80e74a78eb90607e594560d492f6a6585db2e1b3
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-15 21:16:03 +0530

    Update json.py

--- userbot/plugins/json.py
+++ userbot/plugins/json.py
@@ -10,12 +10,12 @@ plugin_category = "tools"
     pattern="json$",
     command=("json", plugin_category),
     info={
-        "header": "To get details of that message in json format.",
+        "header": "To get details of that message in json format",
         "usage": "{tr}json reply to message",
     },
 )
 async def _(event):
-    "To get details of that message in json format."
+    "To get details of that message in json format"
     catevent = await event.get_reply_message() if event.reply_to_msg_id else event
     the_real_message = catevent.stringify()
     await edit_or_reply(event, the_real_message, parse_mode=_format.parse_pre)
@@ -25,12 +25,12 @@ async def _(event):
     pattern="yaml$",
     command=("yaml", plugin_category),
     info={
-        "header": "To get details of that message in yaml format.",
+        "header": "To get details of that message in yaml format",
         "usage": "{tr}yaml reply to message",
     },
 )
 async def _(event):
-    "To get details of that message in yaml format."
+    "To get details of that message in yaml format"
     catevent = await event.get_reply_message() if event.reply_to_msg_id else event
     the_real_message = _format.yaml_format(catevent)
     await edit_or_reply(event, the_real_message, parse_mode=_format.parse_pre)
