59bc5d4b5f77d398b88796e9f5c229ddacf26548
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-17 12:57:38 +0530

    Update mediainfo.py

--- userbot/plugins/mediainfo.py
+++ userbot/plugins/mediainfo.py
@@ -17,35 +17,35 @@ LOGS = logging.getLogger(__name__)
 async def file_data(reply):
     hmm = ""
     if reply.file.name:
-        hmm += f"Name  :  {reply.file.name}<br>"
+        hmm += f"Name :  {reply.file.name}<br>"
     if reply.file.mime_type:
-        hmm += f"Mime type  :  {reply.file.mime_type}<br>"
+        hmm += f"Mime type :  {reply.file.mime_type}<br>"
     if reply.file.size:
-        hmm += f"Size  :  {humanbytes(reply.file.size)}<br>"
+        hmm += f"Size :  {humanbytes(reply.file.size)}<br>"
     if reply.date:
-        hmm += f"Date  :  {_format.yaml_format(reply.date)}<br>"
+        hmm += f"Date :  {_format.yaml_format(reply.date)}<br>"
     if reply.file.id:
-        hmm += f"Id  :  {reply.file.id}<br>"
+        hmm += f"Id :  {reply.file.id}<br>"
     if reply.file.ext:
-        hmm += f"Extension  :  '{reply.file.ext}'<br>"
+        hmm += f"Extension :  '{reply.file.ext}'<br>"
     if reply.file.emoji:
-        hmm += f"Emoji  :  {reply.file.emoji}<br>"
+        hmm += f"Emoji :  {reply.file.emoji}<br>"
     if reply.file.title:
-        hmm += f"Title  :  {reply.file.title}<br>"
+        hmm += f"Title :  {reply.file.title}<br>"
     if reply.file.performer:
-        hmm += f"Performer  :  {reply.file.performer}<br>"
+        hmm += f"Performer :  {reply.file.performer}<br>"
     if reply.file.duration:
-        hmm += f"Duration  :  {reply.file.duration} seconds<br>"
+        hmm += f"Duration :  {reply.file.duration} seconds<br>"
     if reply.file.height:
         hmm += f"Height :  {reply.file.height}<br>"
     if reply.file.width:
-        hmm += f"Width  :  {reply.file.width}<br>"
+        hmm += f"Width :  {reply.file.width}<br>"
     if reply.file.sticker_set:
-        hmm += f"Sticker set  :\
+        hmm += f"Sticker set :\
             \n {_format.yaml_format(reply.file.sticker_set)}<br>"
     try:
         if reply.media.document.thumbs:
-            hmm += f"Thumb  :\
+            hmm += f"Thumb :\
                 \n {_format.yaml_format(reply.media.document.thumbs[-1])}<br>"
     except Exception as e:
         LOGS.info(str(e))
@@ -56,8 +56,8 @@ async def file_data(reply):
     pattern="minfo$",
     command=("minfo", plugin_category),
     info={
-        "header": "To get media information.",
-        "description": "reply to media to get information about it",
+        "header": "To get media information",
+        "description": "Reply to media to get information about it",
         "usage": "{tr}minfo",
     },
 )
@@ -66,20 +66,20 @@ async def mediainfo(event):
     X_MEDIA = None
     reply = await event.get_reply_message()
     if not reply:
-        await edit_or_reply(event, "reply to media to get info")
+        await edit_or_reply(event, "Reply to media to get info")
         return
     if not reply.media:
-        await edit_or_reply(event, "reply to media to get info")
+        await edit_or_reply(event, "Reply to media to get info")
         return
-    catevent = await edit_or_reply(event, "`Gathering ...`")
+    catevent = await edit_or_reply(event, "`Gathering...`")
     X_MEDIA = reply.file.mime_type
     if (not X_MEDIA) or (X_MEDIA.startswith(("text"))):
-        return await catevent.edit("Reply To a supported Media Format")
+        return await catevent.edit("Reply to a supported media format")
     hmm = await file_data(reply)
     file_path = await reply.download_media(Config.TEMP_DIR)
     out, err, ret, pid = await _catutils.runcmd(f"mediainfo '{file_path}'")
     if not out:
-        out = "Not Supported"
+        out = "Not supported"
     body_text = f"""
 <h2>JSON</h2>
 <code>
@@ -91,7 +91,7 @@ async def mediainfo(event):
 </code>"""
     link = await post_to_telegraph(f"{X_MEDIA}", body_text)
     await catevent.edit(
-        f"ℹ️  <b>MEDIA INFO:  <a href ='{link}' > {X_MEDIA}</a></b>",
+        f"ℹ️  <b>MEDIA INFO :  <a href ='{link}' > {X_MEDIA}</a></b>",
         parse_mode="HTML",
         link_preview=True,
     )
