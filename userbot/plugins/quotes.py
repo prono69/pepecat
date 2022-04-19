6f704e92693dd0333e78290eb841e10945cd6ea1
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-19 10:42:01 +0530

    Update quotes.py

--- userbot/plugins/quotes.py
+++ userbot/plugins/quotes.py
@@ -17,19 +17,19 @@ plugin_category = "extra"
     pattern="quote(?:\s|$)([\s\S]*)",
     command=("quote", plugin_category),
     info={
-        "header": "To get random quotes on given topic.",
-        "description": "An api that Fetchs random Quote from `goodreads.com`",
+        "header": "To get random quotes on given topic",
+        "description": "An api that fetchs random quote from `goodreads.com`",
         "usage": "{tr}quote <topic>",
         "examples": "{tr}quote love",
     },
 )
 async def quote_search(event):
-    "shows random quotes on given topic."
+    "Shows random quotes on given topic"
     input_str = event.pattern_match.group(1)
     try:
         response = await search_quotes(input_str) if input_str else await random_quote()
     except Exception:
-        return await edit_delete(event, "`Sorry Zero results found`", 5)
+        return await edit_delete(event, "`Sorry zero results found`", 5)
     await edit_or_reply(event, response, parse_mode=parse_pre)
 
 
@@ -37,7 +37,7 @@ async def quote_search(event):
     pattern="pquote$",
     command=("pquote", plugin_category),
     info={
-        "header": "To get random quotes on programming.",
+        "header": "To get random quotes on programming",
         "usage": "{tr}pquote",
     },
 )
