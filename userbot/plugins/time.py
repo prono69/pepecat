44711d0dc63d94cf14a576b8b361e71551f35138
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-20 20:16:50 +0530

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -21,7 +21,7 @@ FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 
 
 async def get_tz(con):
-    """Get time zone of the given country."""
+    """Get time zone of the given country"""
     if "(Uk)" in con:
         con = con.replace("Uk", "UK")
     if "(Us)" in con:
@@ -45,13 +45,13 @@ async def get_tz(con):
 
 
 @catub.cat_cmd(
-    pattern="ctime(?:\s|$)([\s\S]*)(?<![0-9])(?: |$)([0-9]+)?",
-    command=("ctime", plugin_category),
+    pattern="time(?:\s|$)([\s\S]*)(?<![0-9])(?: |$)([0-9]+)?",
+    command=("time", plugin_category),
     info={
         "header": "To get current time of a paticular country",
         "note": "For country names check [this link](https://telegra.ph/country-names-10-24)",
-        "usage": "{tr}ctime <country name/code> <timezone number>",
-        "examples": "{tr}ctime Brazil 2",
+        "usage": "{tr}time <country name/code> <timezone number>",
+        "examples": "{tr}time Brazil 2",
     },
 )
 async def time_func(tdata):
@@ -77,7 +77,7 @@ async def time_func(tdata):
             f"`It's`  **{dt.now().strftime(t_form)}**` on `**{dt.now().strftime(d_form)}** `here.`",
         )
     if not timezones:
-        return await edit_or_reply(tdata, "`Invaild country.`")
+        return await edit_or_reply(tdata, "`Invaild country`")
     if len(timezones) == 1:
         time_zone = timezones[0]
     elif len(timezones) > 1:
@@ -85,14 +85,14 @@ async def time_func(tdata):
             tz_num = int(tz_num)
             time_zone = timezones[tz_num - 1]
         else:
-            return_str = f"`{c_name} has multiple timezones:`\n\n"
+            return_str = f"`{c_name} has multiple timezones :`\n\n"
 
             for i, item in enumerate(timezones):
                 return_str += f"`{i+1}. {item}`\n"
 
             return_str += "\n`Choose one by typing the number "
-            return_str += "in the command.`\n"
-            return_str += f"`Example: .ctime {c_name} 2`"
+            return_str += "in the command`\n"
+            return_str += f"`Example : .ctime {c_name} 2`"
 
             return await edit_or_reply(tdata, return_str)
 
@@ -101,37 +101,36 @@ async def time_func(tdata):
     if c_name != Config.COUNTRY:
         await edit_or_reply(
             tdata,
-            f"`It's`  **{dtnow1}**` on `**{dtnow2}**  `in {c_name} ({time_zone} timezone).`",
+            f"`It's`  **{dtnow1}**` on `**{dtnow2}**  `in {c_name} ({time_zone} timezone)`",
         )
     if Config.COUNTRY:
         await edit_or_reply(
             tdata,
-            f"`It's`  **{dtnow1}**` on `**{dtnow2}**  `here, in {Config.COUNTRY}"
-            f"({time_zone} timezone).`",
+            f"`It's`  **{dtnow1}**` on `**{dtnow2}**  `here , in {Config.COUNTRY}"
+            f"({time_zone} timezone)`",
         )
 
 
 @catub.cat_cmd(
-    pattern="time(?:\s|$)([\s\S]*)",
-    command=("time", plugin_category),
+    pattern="(s|p|c)time(?:\s|$)([\s\S]*)",
+    command=("stime", plugin_category),
     info={
-        "header": "To show current time.",
-        "description": "shows current default time you can change by changing TZ in heroku vars.",
-        "usage": "{tr}time",
+        "header": "To show current time",
+        "description": "Shows current default time you can change by changing TZ in heroku vars",
+        "usage": "{tr}stime",
     },
 )
 async def _(event):
     "To show current time"
     reply_msg_id = await reply_id(event)
     current_time = dt.now().strftime(
-        f"⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡\n⚡USERBOT TIMEZONE⚡\n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡\n   {os.path.basename(Config.TZ)}\n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡"
+        f"⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡\n⚡USERBOT TIMEZONE⚡\n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡\n   {os.path.basename(Config.TZ)}\n  Time : %H:%M:%S \n  Date : %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡"
     )
-    input_str = event.pattern_match.group(1)
-    if input_str:
+    if input_str := event.pattern_match.group(2):
         current_time = input_str
     if not os.path.isdir(Config.TEMP_DIR):
         os.makedirs(Config.TEMP_DIR)
-    required_file_name = Config.TEMP_DIR + " " + str(dt.now()) + ".webp"
+    required_file_name = f"{Config.TEMP_DIR} {str(dt.now())}.webp"
     img = Image.new("RGBA", (350, 220), color=(0, 0, 0, 115))
     fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
     drawn_text = ImageDraw.Draw(img)
