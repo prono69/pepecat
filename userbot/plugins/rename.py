44cd042138ceffe783f3d93d6d9135668eef88ee
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-20 17:17:38 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -19,8 +19,8 @@ thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg"
     pattern="rnup ?(-f)? ([\s\S]*)",
     command=("rnup", plugin_category),
     info={
-        "header": "To rename and upload the replied file.",
-        "flags": {"f": "will upload as file that is document not streamable."},
+        "header": "To rename and upload the replied file",
+        "flags": {"f": "Will upload as file that is document not streamable"},
         "description": "If flag is not used then will upload as steamable file",
         "usage": [
             "{tr}rnup <new file name>",
@@ -36,13 +36,13 @@ async def _(event):
     supsstream = not flags
     catevent = await edit_or_reply(
         event,
-        "`Rename & Upload in process 🙄🙇‍♂️🙇‍♂️🙇‍♀️ It might take some time if file size is big`",
+        "`Rename & upload in process ! It might take some time if file size is big`",
     )
     reply_to_id = await reply_id(event)
     input_str = event.pattern_match.group(2)
     if not event.reply_to_msg_id:
         return await catevent.edit(
-            "**Syntax : **`.rnup file name` as reply to a Telegram media"
+            "**Syntax : **`.rnup file name` as reply to a telegram media"
         )
     start = datetime.now()
     file_name = input_str
@@ -53,7 +53,7 @@ async def _(event):
         reply_message,
         downloaded_file_name,
         progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-            progress(d, t, catevent, c_time, "trying to download", file_name)
+            progress(d, t, catevent, c_time, "Trying to download", file_name)
         ),
     )
     end = datetime.now()
@@ -63,7 +63,7 @@ async def _(event):
     except Exception:
         thumb = thumb
     if not os.path.exists(downloaded_file_name):
-        return await catevent.edit(f"File Not Found {input_str}")
+        return await catevent.edit(f"File not found {input_str}")
     c_time = time.time()
     caat = await event.client.send_file(
         event.chat_id,
@@ -74,7 +74,7 @@ async def _(event):
         reply_to=reply_to_id,
         thumb=thumb,
         progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-            progress(d, t, event, c_time, "trying to upload", downloaded_file_name)
+            progress(d, t, event, c_time, "Trying to upload", downloaded_file_name)
         ),
     )
     end_two = datetime.now()
@@ -82,5 +82,5 @@ async def _(event):
     ms_two = (end_two - end).seconds
     await edit_delete(
         catevent,
-        f"`Downloaded file in {ms_one} seconds.\nAnd Uploaded in {ms_two} seconds.`",
+        f"`Downloaded file in {ms_one} seconds\n\nAnd uploaded in {ms_two} seconds`",
     )
