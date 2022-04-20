72a87e6c772d2da5a3bc1d812e8230fb065ab300
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-20 18:30:21 +0530

    Update stickerfun.py

--- userbot/plugins/stickerfun.py
+++ userbot/plugins/stickerfun.py
@@ -2,7 +2,7 @@
 # modified by @UniBorg
 # imported from ppe-remix by @heyworld & @DeletedUser420
 # modified by @mrconfused
-
+# pengin & gandhi Yato
 
 # RegEx by https://t.me/c/1220993104/500653 ( @SnapDragon7410 )
 import io
@@ -51,7 +51,7 @@ async def get_font_file(client, channel_id, search_kw=""):
     pattern="(?:st|sttxt)(?:\s|$)([\s\S]*)",
     command=("sttxt", plugin_category),
     info={
-        "header": "Anime that makes your writing fun.",
+        "header": "Anime that makes your writing fun",
         "usage": "{tr}sttxt <text>",
         "examples": "{tr}sttxt hello",
     },
@@ -65,7 +65,7 @@ async def waifu(animu):
             text = (await animu.get_reply_message()).message
         else:
             return await edit_or_reply(
-                animu, "`You haven't written any article, Waifu is going away.`"
+                animu, "`You haven't written any article , waifu is going away`"
             )
     text = deEmojify(text)
     await animu.delete()
@@ -77,7 +77,7 @@ async def waifu(animu):
     pattern="stcr ?(?:(.*?) ?; )?([\s\S]*)",
     command=("stcr", plugin_category),
     info={
-        "header": "your text as sticker.",
+        "header": "Your text as sticker",
         "usage": [
             "{tr}stcr <text>",
             "{tr}stcr <font file name> ; <text>",
@@ -86,7 +86,7 @@ async def waifu(animu):
     },
 )
 async def sticklet(event):
-    "your text as sticker"
+    "Your text as sticker"
     R = random.randint(0, 256)
     G = random.randint(0, 256)
     B = random.randint(0, 256)
@@ -145,13 +145,13 @@ async def sticklet(event):
     pattern="honk(?:\s|$)([\s\S]*)",
     command=("honk", plugin_category),
     info={
-        "header": "Make honk say anything.",
+        "header": "Make honk say anything",
         "usage": "{tr}honk <text/reply to msg>",
         "examples": "{tr}honk How you doing?",
     },
 )
 async def honk(event):
-    "Make honk say anything."
+    "Make honk say anything"
     text = event.pattern_match.group(1)
     reply_to_id = await reply_id(event)
     bot_name = "@honka_says_bot"
@@ -160,7 +160,7 @@ async def honk(event):
             text = (await event.get_reply_message()).message
         else:
             return await edit_delete(
-                event, "__What is honk supposed to say? Give some text.__"
+                event, "What is honk supposed to say ? Give some text"
             )
     text = deEmojify(text)
     await event.delete()
@@ -177,7 +177,7 @@ async def honk(event):
     },
 )
 async def twt(event):
-    "Make a cool tweet of your account."
+    "Make a cool tweet of your account"
     text = event.pattern_match.group(1)
     reply_to_id = await reply_id(event)
     bot_name = "@TwitterStatusBot"
@@ -186,7 +186,7 @@ async def twt(event):
             text = (await event.get_reply_message()).message
         else:
             return await edit_delete(
-                event, "__What am I supposed to Tweet? Give some text.__"
+                event, "What am I supposed to tweet ? Give some text"
             )
     text = deEmojify(text)
     await event.delete()
@@ -197,7 +197,7 @@ async def twt(event):
     pattern="glax(|r)(?:\s|$)([\s\S]*)",
     command=("glax", plugin_category),
     info={
-        "header": "Make glax the dragon scream your text.",
+        "header": "Make glax the dragon scream your text",
         "flags": {
             "r": "Reverse the face of the dragon",
         },
@@ -206,13 +206,13 @@ async def twt(event):
             "{tr}glaxr <text/reply to msg>",
         ],
         "examples": [
-            "{tr}glax Die you",
-            "{tr}glaxr Die you",
+            "{tr}glax die you",
+            "{tr}glaxr die you",
         ],
     },
 )
 async def glax(event):
-    "Make glax the dragon scream your text."
+    "Make glax the dragon scream your text"
     cmd = event.pattern_match.group(1).lower()
     text = event.pattern_match.group(2)
     reply_to_id = await reply_id(event)
@@ -223,7 +223,7 @@ async def glax(event):
             text = (await event.get_reply_message()).message
         else:
             return await edit_delete(
-                event, "What is glax supposed to scream? Give text.."
+                event, "What is glax supposed to scream ? Give text.."
             )
     text = deEmojify(text)
     await event.delete()
@@ -236,17 +236,17 @@ async def glax(event):
     pattern="(|b)quby(?:\s|$)([\s\S]*)",
     command=("quby", plugin_category),
     info={
-        "header": "Make doge say anything.",
+        "header": "Make doge say anything",
         "flags": {
-            "b": "Give the sticker on background.",
+            "b": "Give the sticker on background",
         },
         "usage": [
             "{tr}quby <text/reply to msg>",
             "{tr}bquby <text/reply to msg>",
         ],
         "examples": [
-            "{tr}quby Gib money",
-            "{tr}bquby Gib money",
+            "{tr}quby give money",
+            "{tr}bquby give money",
         ],
     },
 )
@@ -259,13 +259,12 @@ async def quby(event):
         text = (await event.get_reply_message()).message
     if not text:
         return await edit_delete(
-            event, "__What is quby supposed to say? Give some text.__"
+            event, "What is quby supposed to say ? Give some text"
         )
-    await edit_delete(event, "`Wait, processing.....`")
+    await edit_delete(event, "`Wait , processing...`")
     if not os.path.isdir("./temp"):
         os.mkdir("./temp")
     temp_name = "./temp/quby_temp.png"
-    file_name = "./temp/quby.png"
     templait = urllib.request.urlretrieve(
         "https://telegra.ph/file/09f4df5a129758a2e1c9c.jpg", temp_name
     )
@@ -278,24 +277,23 @@ async def quby(event):
         wrap = 1.2
         position = (0, 0)
     text = deEmojify(text)
-    higlighted_text(
+    file = higlighted_text(
         temp_name,
         text,
-        file_name,
         text_wrap=wrap,
         font_size=font,
         linespace="+4",
         position=position,
     )
     if cmd == "b":
-        cat = convert_tosticker(file_name)
+        cat = convert_tosticker(file[0])
         await event.client.send_file(
             event.chat_id, cat, reply_to=reply_to_id, force_document=False
         )
     else:
-        await clippy(event.client, file_name, event.chat_id, reply_to_id)
+        await clippy(event.client, file[0], event.chat_id, reply_to_id)
     await event.delete()
-    for files in (temp_name, file_name):
+    for files in (temp_name, file[0]):
         if files and os.path.exists(files):
             os.remove(files)
 
@@ -304,17 +302,17 @@ async def quby(event):
     pattern="(|b)blob(?:\s|$)([\s\S]*)",
     command=("blob", plugin_category),
     info={
-        "header": "Give the sticker on background.",
+        "header": "Give the sticker on background",
         "flags": {
-            "b": "To create knife sticker transparent.",
+            "b": "To create knife sticker transparent",
         },
         "usage": [
             "{tr}blob <text/reply to msg>",
             "{tr}bblob <text/reply to msg>",
         ],
         "examples": [
-            "{tr}blob Gib money",
-            "{tr}bblob Gib money",
+            "{tr}blob give money",
+            "{tr}bblob give money",
         ],
     },
 )
@@ -327,13 +325,12 @@ async def knife(event):
         text = (await event.get_reply_message()).message
     if not text:
         return await edit_delete(
-            event, "__What is knife supposed to say? Give some text.__"
+            event, "What is knife supposed to say ? Give some text"
         )
-    await edit_delete(event, "`Wait, processing.....`")
+    await edit_delete(event, "`Wait , processing...`")
     if not os.path.isdir("./temp"):
         os.mkdir("./temp")
     temp_name = "./temp/knife_temp.png"
-    file_name = "./temp/knife.png"
     templait = urllib.request.urlretrieve(
         "https://telegra.ph/file/2188367c8c5f43c36aa59.jpg", temp_name
     )
@@ -346,10 +343,9 @@ async def knife(event):
         wrap = 1.4
         position = (150, 500)
     text = deEmojify(text)
-    higlighted_text(
+    file = higlighted_text(
         temp_name,
         text,
-        file_name,
         text_wrap=wrap,
         font_size=font,
         linespace="-5",
@@ -357,14 +353,14 @@ async def knife(event):
         direction="upwards",
     )
     if cmd == "b":
-        cat = convert_tosticker(file_name)
+        cat = convert_tosticker(file[0])
         await event.client.send_file(
             event.chat_id, cat, reply_to=reply_to_id, force_document=False
         )
     else:
-        await clippy(event.client, file_name, event.chat_id, reply_to_id)
+        await clippy(event.client, file[0], event.chat_id, reply_to_id)
     await event.delete()
-    for files in (temp_name, file_name):
+    for files in (temp_name, file[0]):
         if files and os.path.exists(files):
             os.remove(files)
 
@@ -373,17 +369,17 @@ async def knife(event):
     pattern="(|h)doge(?:\s|$)([\s\S]*)",
     command=("doge", plugin_category),
     info={
-        "header": "Make doge say anything.",
+        "header": "Make doge say anything",
         "flags": {
-            "h": "To create doge sticker with highligted text.",
+            "h": "To create doge sticker with highligted text",
         },
         "usage": [
             "{tr}doge <text/reply to msg>",
             "{tr}tdoge <text/reply to msg>",
         ],
         "examples": [
-            "{tr}doge Gib money",
-            "{tr}hdoge Gib money",
+            "{tr}doge give money",
+            "{tr}hdoge give money",
         ],
     },
 )
@@ -396,13 +392,12 @@ async def doge(event):
         text = (await event.get_reply_message()).message
     if not text:
         return await edit_delete(
-            event, "__What is doge supposed to say? Give some text.__"
+            event, "What is doge supposed to say ? Give some text"
         )
-    await edit_delete(event, "`Wait, processing.....`")
+    await edit_delete(event, "`Wait , processing...`")
     if not os.path.isdir("./temp"):
         os.mkdir("./temp")
     temp_name = "./temp/doge_temp.jpg"
-    file_name = "./temp/doge.jpg"
     templait = urllib.request.urlretrieve(
         "https://telegra.ph/file/6f621b9782d9c925bd6c4.jpg", temp_name
     )
@@ -411,10 +406,9 @@ async def doge(event):
     bg, fg, alpha, ls = (
         ("black", "white", 255, "5") if cmd == "h" else ("white", "black", 0, "-40")
     )
-    higlighted_text(
+    file = higlighted_text(
         temp_name,
         text,
-        file_name,
         text_wrap=wrap,
         font_size=font,
         linespace=ls,
@@ -424,11 +418,137 @@ async def doge(event):
         foreground=fg,
         transparency=alpha,
     )
-    cat = convert_tosticker(file_name)
+    cat = convert_tosticker(file[0])
     await event.client.send_file(
         event.chat_id, cat, reply_to=reply_to_id, force_document=False
     )
     await event.delete()
-    for files in (temp_name, file_name):
+    for files in (temp_name, file[0]):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+@catub.cat_cmd(
+    pattern="(|h)penguin(?:\s|$)([\s\S]*)",
+    command=("penguin", plugin_category),
+    info={
+        "header": "To make penguin meme sticker ",
+        "flags": {
+            "h": "To create penguin sticker with highligted text",
+        },
+        "usage": [
+            "{tr}penguin <text/reply to msg>",
+            "{tr}hpenguin <text/reply to msg>",
+        ],
+        "examples": [
+            "{tr}penguin shut up rash",
+            "{tr}hpenguin shut up rash",
+        ],
+    },
+)
+async def penguin(event):
+    "Make a cool penguin text sticker"
+    cmd = event.pattern_match.group(1).lower()
+    text = event.pattern_match.group(2)
+    reply_to_id = await reply_id(event)
+    if not text and event.is_reply:
+        text = (await event.get_reply_message()).message
+    if not text:
+        return await edit_delete(
+            event, "What is penguin supposed to say ? Give some text"
+        )
+    await edit_delete(event, "Wait , processing...")
+    if not os.path.isdir("./temp"):
+        os.mkdir("./temp")
+    temp_name = "./temp/peguin_temp.jpg"
+    templait = urllib.request.urlretrieve(
+        "https://telegra.ph/file/ee1fc91bbaef2cc808c7c.png", temp_name
+    )
+    text = deEmojify(text)
+    font, wrap = (90, 4) if len(text) < 50 else (70, 4.5)
+    bg, fg, alpha, ls = (
+        ("black", "white", 255, "-20") if cmd == "h" else ("white", "black", 0, "-40")
+    )
+    file = higlighted_text(
+        temp_name,
+        text,
+        text_wrap=wrap,
+        font_size=font,
+        linespace=ls,
+        position=(0, 10),
+        align="left",
+        background=bg,
+        foreground=fg,
+        transparency=alpha,
+    )
+    cat = convert_tosticker(file[0])
+    await event.client.send_file(
+        event.chat_id, cat, reply_to=reply_to_id, force_document=False
+    )
+    await event.delete()
+    for files in (temp_name, file[0]):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+@catub.cat_cmd(
+    pattern="(|h)gandhi(?:\s|$)([\s\S]*)",
+    command=("gandhi", plugin_category),
+    info={
+        "header": "Make gandhi text sticker",
+        "flags": {
+            "h": "To create gandhi sticker with highligted text",
+        },
+        "usage": [
+            "{tr}gandhi <text/reply to msg>",
+            "{tr}hgandhi <text/reply to msg>",
+        ],
+        "examples": [
+            "{tr}gandhi nathu killed me",
+            "{tr}hgandhi nathu killed me",
+        ],
+    },
+)
+async def gandhi(event):
+    "Make a cool gandhi text sticker"
+    cmd = event.pattern_match.group(1).lower()
+    text = event.pattern_match.group(2)
+    reply_to_id = await reply_id(event)
+    if not text and event.is_reply:
+        text = (await event.get_reply_message()).message
+    if not text:
+        return await edit_delete(
+            event, "What is gandhi supposed to write ? Give some text"
+        )
+    await edit_delete(event, "Wait , processing...")
+    if not os.path.isdir("./temp"):
+        os.mkdir("./temp")
+    temp_name = "./temp/gandhi_temp.jpg"
+    templait = urllib.request.urlretrieve(
+        "https://telegra.ph/file/3bebc56ee82cce4f300ce.jpg", temp_name
+    )
+    text = deEmojify(text)
+    font, wrap = (90, 3) if len(text) < 60 else (70, 2.8)
+    bg, fg, alpha, ls = (
+        ("white", "black", 255, "-20") if cmd == "h" else ("black", "white", 0, "-40")
+    )
+    file = higlighted_text(
+        temp_name,
+        text,
+        text_wrap=wrap,
+        font_size=font,
+        linespace=ls,
+        position=(470, 10),
+        align="center",
+        background=bg,
+        foreground=fg,
+        transparency=alpha,
+    )
+    cat = convert_tosticker(file[0])
+    await event.client.send_file(
+        event.chat_id, cat, reply_to=reply_to_id, force_document=False
+    )
+    await event.delete()
+    for files in (temp_name, file[0]):
         if files and os.path.exists(files):
             os.remove(files)
