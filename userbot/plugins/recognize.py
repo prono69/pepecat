948fbd955db37de55688e8cc2a758dcccf184131
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-20 16:49:41 +0530

    Update recognize.py

--- userbot/plugins/recognize.py
+++ userbot/plugins/recognize.py
@@ -14,20 +14,20 @@ plugin_category = "utils"
     command=("recognize", plugin_category),
     info={
         "header": "To recognize a image",
-        "description": "Get information about an image using AWS Rekognition. Find out information including detected labels, faces. text and moderation tags",
+        "description": "Get information about an image using aws rekognition ! Find out information including detected labels , faces , text and moderation tags",
         "usage": "{tr}recognize",
     },
 )
 async def _(event):
-    "To recognize a image."
+    "To recognize a image"
     if not event.reply_to_msg_id:
-        return await edit_or_reply(event, "Reply to any user's media message.")
+        return await edit_or_reply(event, "Reply to any user's media message")
     reply_message = await event.get_reply_message()
     if not reply_message.media:
         return await edit_or_reply(event, "reply to media file")
     chat = "@Rekognition_Bot"
     if reply_message.sender.bot:
-        return await event.edit("Reply to actual users message.")
+        return await event.edit("Reply to actual users message")
     cat = await edit_or_reply(event, "recognizeing this media")
     async with event.client.conversation(chat) as conv:
         try:
@@ -39,7 +39,7 @@ async def _(event):
         except YouBlockedUserError:
             await cat.edit("unblock @Rekognition_Bot and try again")
             return
-        if response.text.startswith("See next message."):
+        if response.text.startswith("See next message"):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=461083923)
             )
@@ -47,5 +47,5 @@ async def _(event):
             msg = response.message.message
             await cat.edit(msg)
         else:
-            await cat.edit("sorry, I couldnt find it")
+            await cat.edit("Sorry , I couldn't find it")
         await event.client.send_read_acknowledge(conv.chat_id)
