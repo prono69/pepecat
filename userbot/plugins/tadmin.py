b1d871fdd7322d25c9790b8570cfb5f339e6af03
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-20 19:39:43 +0530

    Update tadmin.py

--- userbot/plugins/tadmin.py
+++ userbot/plugins/tadmin.py
@@ -17,8 +17,8 @@ from . import BOTLOG, BOTLOG_CHATID, extract_time, get_user_from_event
 plugin_category = "admin"
 
 # =================== CONSTANT ===================
-NO_ADMIN = "`I am not an admin nub nibba!`"
-NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play despacito`"
+NO_ADMIN = "`I am not an admin stupid kid`"
+NO_PERM = "`I don't have sufficient permissions ! This is so sad alexa play despacito`"
 
 
 @catub.cat_cmd(
@@ -26,13 +26,13 @@ NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play desp
     command=("tmute", plugin_category),
     info={
         "header": "To stop sending messages permission for that user",
-        "description": "Temporary mutes the user for given time.",
+        "description": "Temporary mutes the user for given time",
         "Time units": {
-            "s": "seconds",
-            "m": "minutes",
+            "s": "Seconds",
+            "m": "Minutes",
             "h": "Hours",
-            "d": "days",
-            "w": "weeks",
+            "d": "Days",
+            "w": "Weeks",
         },
         "usage": [
             "{tr}tmute <userid/username/reply> <time>",
@@ -45,12 +45,12 @@ NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play desp
 )
 async def tmuter(event):  # sourcery no-metrics
     "To mute a person for specific time"
-    catevent = await edit_or_reply(event, "`muting....`")
+    catevent = await edit_or_reply(event, "`Muting...`")
     user, reason = await get_user_from_event(event, catevent)
     if not user:
         return
     if not reason:
-        return await catevent.edit("you haven't mentioned time, check `.help tmute`")
+        return await catevent.edit("You haven't mentioned time , check `.help tmute`")
     reason = reason.split(" ", 1)
     hmm = len(reason)
     cattime = reason[0].strip()
@@ -59,7 +59,7 @@ async def tmuter(event):  # sourcery no-metrics
     if not ctime:
         return
     if user.id == event.client.uid:
-        return await catevent.edit("Sorry, I can't mute myself")
+        return await catevent.edit("Sorry , I can't mute myself")
     try:
         await catevent.client(
             EditBannedRequest(
@@ -73,12 +73,12 @@ async def tmuter(event):  # sourcery no-metrics
             await catevent.edit(
                 f"{_format.mentionuser(user.first_name ,user.id)} was muted in {get_display_name(await event.get_chat())}\n"
                 f"**Muted for : **{cattime}\n"
-                f"**Reason : **__{reason}__"
+                f"**Reason : **{reason}"
             )
             if BOTLOG:
                 await event.client.send_message(
                     BOTLOG_CHATID,
-                    "#TMUTE\n"
+                    "TMUTE\n"
                     f"**User : **[{user.first_name}](tg://user?id={user.id})\n"
                     f"**Chat : **{get_display_name(await event.get_chat())}(`{event.chat_id}`)\n"
                     f"**Muted for : **`{cattime}`\n"
@@ -92,14 +92,14 @@ async def tmuter(event):  # sourcery no-metrics
             if BOTLOG:
                 await event.client.send_message(
                     BOTLOG_CHATID,
-                    "#TMUTE\n"
+                    "TMUTE\n"
                     f"**User : **[{user.first_name}](tg://user?id={user.id})\n"
                     f"**Chat : **{get_display_name(await event.get_chat())}(`{event.chat_id}`)\n"
                     f"**Muted for : **`{cattime}`",
                 )
         # Announce to logging group
     except UserIdInvalidError:
-        return await catevent.edit("`Uh oh my mute logic broke!`")
+        return await catevent.edit("`Oh my mute logic broke !`")
     except UserAdminInvalidError:
         return await catevent.edit(
             "`Either you're not an admin or you tried to mute an admin that you didn't promote`"
@@ -112,14 +112,14 @@ async def tmuter(event):  # sourcery no-metrics
     pattern="tban(?:\s|$)([\s\S]*)",
     command=("tban", plugin_category),
     info={
-        "header": "To remove a user from the group for specified time.",
-        "description": "Temporary bans the user for given time.",
+        "header": "To remove a user from the group for specified time",
+        "description": "Temporary bans the user for given time",
         "Time units": {
-            "s": "seconds",
-            "m": "minutes",
+            "s": "Seconds",
+            "m": "Minutes",
             "h": "Hours",
-            "d": "days",
-            "w": "weeks",
+            "d": "Days",
+            "w": "Weeks",
         },
         "usage": [
             "{tr}tban <userid/username/reply> <time>",
@@ -132,12 +132,12 @@ async def tmuter(event):  # sourcery no-metrics
 )
 async def tban(event):  # sourcery no-metrics
     "To ban a person for specific time"
-    catevent = await edit_or_reply(event, "`banning....`")
+    catevent = await edit_or_reply(event, "`Banning...`")
     user, reason = await get_user_from_event(event, catevent)
     if not user:
         return
     if not reason:
-        return await catevent.edit("you haven't mentioned time, check `.help tban`")
+        return await catevent.edit("You haven't mentioned time , check `.help tban`")
     reason = reason.split(" ", 1)
     hmm = len(reason)
     cattime = reason[0].strip()
@@ -146,8 +146,8 @@ async def tban(event):  # sourcery no-metrics
     if not ctime:
         return
     if user.id == event.client.uid:
-        return await catevent.edit("Sorry, I can't ban myself")
-    await catevent.edit("`Whacking the pest!`")
+        return await catevent.edit("Sorry , I can't ban myself")
+    await catevent.edit("`Whacking the pest !`")
     try:
         await event.client(
             EditBannedRequest(
@@ -169,7 +169,7 @@ async def tban(event):  # sourcery no-metrics
             await reply.delete()
     except BadRequestError:
         return await catevent.edit(
-            "`I dont have message nuking rights! But still he was banned!`"
+            "`I dont have message nuking rights ! But still he was banned !`"
         )
     # Delete message and then tell that the command
     # is done gracefully
@@ -178,16 +178,16 @@ async def tban(event):  # sourcery no-metrics
         await catevent.edit(
             f"{_format.mentionuser(user.first_name ,user.id)} was banned in {get_display_name(await event.get_chat())}\n"
             f"banned for {cattime}\n"
-            f"Reason:`{reason}`"
+            f"Reason :`{reason}`"
         )
         if BOTLOG:
             await event.client.send_message(
                 BOTLOG_CHATID,
-                "#TBAN\n"
+                "TBAN\n"
                 f"**User : **[{user.first_name}](tg://user?id={user.id})\n"
                 f"**Chat : **{get_display_name(await event.get_chat())}(`{event.chat_id}`)\n"
-                f"**Banned untill : **`{cattime}`\n"
-                f"**Reason : **__{reason}__",
+                f"**Banned until : **`{cattime}`\n"
+                f"**Reason : **{reason}",
             )
     else:
         await catevent.edit(
@@ -197,8 +197,8 @@ async def tban(event):  # sourcery no-metrics
         if BOTLOG:
             await event.client.send_message(
                 BOTLOG_CHATID,
-                "#TBAN\n"
+                "TBAN\n"
                 f"**User : **[{user.first_name}](tg://user?id={user.id})\n"
                 f"**Chat : **{get_display_name(await event.get_chat())}(`{event.chat_id}`)\n"
-                f"**Banned untill : **`{cattime}`",
+                f"**Banned until : **`{cattime}`",
             )
