a1567e1c42344ad81d633723985e9c5a25fd056e
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-21 11:12:42 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,6 +8,7 @@ from .core.logger import logging
 from .core.session import catub
 from .utils import (
     add_bot_to_logger_group,
+    install_externalrepo,
     load_plugins,
     setup_bot,
     startupmessage,
@@ -17,14 +18,14 @@ from .utils import (
 LOGS = logging.getLogger("CatUserbot")
 
 print(userbot.__copyright__)
-print("Licensed under the terms of the " + userbot.__license__)
+print(f"Licensed under the terms of the {userbot.__license__}")
 
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
     LOGS.info("Starting Userbot")
     catub.loop.run_until_complete(setup_bot())
-    LOGS.info("TG Bot Startup Completed")
+    LOGS.info("TG bot startup completed")
 except Exception as e:
     LOGS.error(f"{e}")
     sys.exit()
@@ -35,10 +36,10 @@ async def startup_process():
     await load_plugins("plugins")
     await load_plugins("assistant")
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-    print("Yay your userbot is officially working.!!!")
+    print("Yay your userbot is officially working !!")
     print(
-        f"Congratulation, now type {cmdhr}alive to see message if catub is live\
-        \nIf you need assistance, head to https://t.me/catuserbot_support"
+        f"Congratulations , now type {cmdhr}alive to see message if catub is live\
+        \nIf you need assistance , head to https://t.me/catuserbot_support"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
@@ -49,13 +50,25 @@ async def startup_process():
     return
 
 
+async def externalrepo():
+    if Config.EXTERNAL_REPO:
+        await install_externalrepo(
+            Config.EXTERNAL_REPO, Config.EXTERNAL_REPOBRANCH, "xtraplugins"
+        )
+    if Config.BADCAT:
+        await install_externalrepo(
+            Config.BADCAT_REPO, Config.BADCAT_REPOBRANCH, "badcatext"
+        )
+
+
 catub.loop.run_until_complete(startup_process())
 
+catub.loop.run_until_complete(externalrepo())
 
-if len(sys.argv) not in (1, 3, 4):
-    catub.disconnect()
-else:
+if len(sys.argv) in {1, 3, 4}:
     try:
         catub.run_until_disconnected()
     except ConnectionError:
         pass
+else:
+    catub.disconnect()
