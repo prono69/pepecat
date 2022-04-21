da2f69421acd50358cfbb02db69d46df57967684
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-21 11:09:00 +0530

    Update nsfw.py

--- userbot/assistant/nsfw.py
+++ userbot/assistant/nsfw.py
@@ -17,19 +17,19 @@ async def age_verification_true(event: CallbackQuery):
     u_id = event.query.user_id
     if u_id != Config.OWNER_ID and u_id not in Config.SUDO_USERS:
         return await event.answer(
-            "Given That It's A Stupid-Ass Decision, I've Elected To Ignore It.",
+            "Given that it's a stupid-ass decision , I've elected to ignore it",
             alert=True,
         )
     await event.answer("Yes I'm 18+", alert=False)
     buttons = [
         Button.inline(
-            text="Unsure / Change of Decision ❔",
+            text="Unsure or change of decision ❔",
             data="chg_of_decision_",
         )
     ]
     try:
         await event.edit(
-            text="Set `ALLOW_NSFW` = True in Database Vars to access this plugin",
+            text="To access this plugin type `.setdv ALLOW_NSFW True`",
             file="https://telegra.ph/file/85f3071c31279bcc280ef.jpg",
             buttons=buttons,
         )
@@ -42,19 +42,19 @@ async def age_verification_false(event: CallbackQuery):
     u_id = event.query.user_id
     if u_id != Config.OWNER_ID and u_id not in Config.SUDO_USERS:
         return await event.answer(
-            "Given That It's A Stupid-Ass Decision, I've Elected To Ignore It.",
+            "Given that it's a stupid-ass decision , I've elected to ignore it",
             alert=True,
         )
-    await event.answer("No I'm Not", alert=False)
+    await event.answer("No I'm not", alert=False)
     buttons = [
         Button.inline(
-            text="Unsure / Change of Decision ❔",
+            text="Unsure or change of decision ❔",
             data="chg_of_decision_",
         )
     ]
     try:
         await event.edit(
-            text="GO AWAY KID !",
+            text="Go away kid !",
             file="https://telegra.ph/file/1140f16a883d35224e6a1.jpg",
             buttons=buttons,
         )
@@ -67,14 +67,14 @@ async def chg_of_decision_(event: CallbackQuery):
     u_id = event.query.user_id
     if u_id != Config.OWNER_ID and u_id not in Config.SUDO_USERS:
         return await event.answer(
-            "Given That It's A Stupid-Ass Decision, I've Elected To Ignore It.",
+            "Given that it's a stupid-ass decision , I've elected to ignore it",
             alert=True,
         )
     await event.answer("Unsure", alert=False)
     buttons = [
         (
             Button.inline(text="Yes I'm 18+", data="age_verification_true"),
-            Button.inline(text="No I'm Not", data="age_verification_false"),
+            Button.inline(text="No I'm not", data="age_verification_false"),
         )
     ]
     try:
