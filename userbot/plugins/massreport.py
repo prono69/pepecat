2f1d28fd107fe5737b16b729dfff5eef4e33dfef
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-04-23 08:39:25 +0530

    Update massreport.py

--- userbot/plugins/massreport.py
+++ userbot/plugins/massreport.py
@@ -15,16 +15,47 @@ plugin_category = "admin"
         "usage": "{tr}massreport <username/reply>",
     },
 )
-async def anim(report):
-    "Mass reports the user"
-    
-    a = 1
-
-    if a == 1 :
-        msrprt = await edit_or_reply(report, "`Initiating mass report of the `[user](tg://user?id={user.id})`")
-        time.sleep(3)
-        reporti = await msrprt.edit(f"[{user.first_name}](tg://user?id={user.id}) `was mass reported in {cattaken} seconds`\n\n**Reason :** `{reason}`")
-        time.sleep(11)
-        await reporti.edit(f"[{user.first_name}](tg://user?id={user.id}) `was mass reported in {cattaken} seconds`")
+async def catgban(event):  # sourcery no-metrics
+    "To ban user in every group where you are admin."
+    cate = await edit_or_reply(event, "`Mass reporting...`")
+    start = datetime.now()
+    user, reason = await get_user_from_event(event, cate)
+    if not user:
+        return
+    if user.id == catub.uid:
+        return await edit_delete(cate, "`Why would I mass report myself ?`")
+    if gban_sql.is_gbanned(user.id):
+        await cate.edit(
+            f"`the `[user](tg://user?id={user.id})` is already in mass report list any way checking again`"
+        )
     else:
-    	await edit_or_reply(f"Seems like something's wrong")
+        gban_sql.catgban(user.id, reason)
+    san = await admin_groups(event.client)
+    count = 0
+    sandy = len(san)
+    if sandy == 0:
+        return await edit_delete(cate, "`You are not admin of atleast one group` ")
+    await cate.edit(
+        f"`Initiating mass report of the `[user](tg://user?id={user.id}) `in {len(san)} groups`"
+    )
+    for i in range(sandy):
+        try:
+            await event.client(EditBannedRequest(san[i], user.id, BANNED_RIGHTS))
+            await asyncio.sleep(0.5)
+            count += 1
+        except BadRequestError:
+            achat = await event.client.get_entity(san[i])
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"`You don't have required permission in :`\n**Chat :** {get_display_name(achat)}(`{achat.id}`)\n`For banning here`",
+            )
+    end = datetime.now()
+    cattaken = (end - start).seconds
+    if reason:
+        await cate.edit(
+            f"[{user.first_name}](tg://user?id={user.id}) `was mass reported in {cattaken} seconds`\n**Reason :** `{reason}`"
+        )
+    else:
+        await cate.edit(
+            f"[{user.first_name}](tg://user?id={user.id}) `was mass reported in {cattaken} seconds`"
+        )
